0c7e5a2d5add569920507830772a7ea4143f569f
zrufo <zrufo747@outlook.com>
2019-10-12 16:21:18 +0800

    日志清理

--- writer_sql.go
+++ writer_sql.go
@@ -17,6 +17,7 @@ type WriterSQL struct {
 	db           *gorm.DB
 	logChan      chan *Log
 	logTableName string
+	logAliveTime time.Duration
 }
 
 // Open initialize a new db connection, need to import driver first, e.g:
@@ -40,14 +41,22 @@ func NewWriterSQL(dialect, connStr, logTableName string) *WriterSQL {
 	if err := db.AutoMigrate(&Log{tableName: logTableName}).Error; err != nil {
 		panic(err)
 	}
-
+	w.logAliveTime = time.Hour * 24 * 365 * 20
 	w.db = db
 	w.logChan = make(chan *Log, 1)
 	w.logTableName = logTableName
 	go w.serve()
+	go func() {
+		for {
+			w.clearLoop()
+			time.Sleep(time.Hour)
+		}
+	}()
 	return w
 }
-
+func (w *WriterSQL) SetLogAliveTime(duration time.Duration) {
+	w.logAliveTime = duration
+}
 func (w *WriterSQL) Write(log *Log) error {
 	w.logChan <- log
 	return nil
@@ -92,15 +101,28 @@ loop:
 		args = append(args, v.Time, v.Trace, v.Level, v.Code, v.OptUserId, goutils.StringChineseMaxLen(v.Msg, 400), v.ExecTime)
 	}
 	sqlStr = strings.TrimRight(sqlStr, ",")
+	beginTime := time.Now()
 
 	db = db.Exec(sqlStr, args...)
+
+	endTime := time.Now()
 
 	err := db.Error
 	if err != nil {
 		panic(err)
 	}
 	insertCount := db.RowsAffected
+	execTime := endTime.Sub(beginTime)
+	log2.Println(fmt.Sprintf("%d records,insert %d to db, use ", recordCount, insertCount), execTime)
 
-	log2.Println(fmt.Sprintf("%d records,insert %d to db, ", recordCount, insertCount))
+}
+func (w *WriterSQL) clearLoop() {
+	deleteTime := time.Now().Add(-w.logAliveTime)
 
+	db := w.db.Where("time < ?", deleteTime).Delete(&Log{tableName: w.logTableName})
+	if db.Error != nil {
+		log2.Println(db.Error)
+	} else {
+		log2.Println(fmt.Sprintf("clear %d rows", db.RowsAffected))
+	}
 }
