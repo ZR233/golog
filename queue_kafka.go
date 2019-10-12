2b50c62d85413a4d4f646638e0a4ed8cec163f4a
zrufo <zrufo747@outlook.com>
2019-10-12 14:47:28 +0800

    日志储存 bug 2

--- queue_kafka.go
+++ queue_kafka.go
@@ -6,11 +6,11 @@ package golog
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/Shopify/sarama"
-	"io/ioutil"
+	cluster "github.com/bsm/sarama-cluster"
 	"os"
-	"strconv"
+	"strings"
+	"time"
 )
 
 type QueueProducerKafka struct {
@@ -55,15 +55,15 @@ func (l *QueueProducerKafka) Receive(logRecord *Log) (err error) {
 }
 
 type QueueConsumerKafka struct {
-	conn              sarama.Consumer
+	conn              *cluster.Consumer
 	partitionConsumer sarama.PartitionConsumer
 	logChan           chan *Log
 	signals           chan int
 	offsetFile        *os.File
 }
 
-func NewQueueConsumerKafka(addrs []string, topic string) *QueueConsumerKafka {
-	r := &QueueConsumerKafka{
+func NewQueueConsumerKafka(addrs []string, topic string) (q *QueueConsumerKafka, err error) {
+	q = &QueueConsumerKafka{
 		logChan: make(chan *Log, 1),
 		signals: make(chan int, 1),
 	}
@@ -74,51 +74,76 @@ func NewQueueConsumerKafka(addrs []string, topic string) *QueueConsumerKafka {
 	}
 	println("-----      end      -----")
 	println("kafka topic: ", topic)
-	f, err := os.OpenFile("log_offset", os.O_RDWR|os.O_CREATE, 0766)
+	//f, err := os.OpenFile("log_offset", os.O_RDWR|os.O_CREATE, 0766)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//r.offsetFile = f
+	//
+	//offset := 0
+	//data, err := ioutil.ReadAll(f)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//n, err := strconv.Atoi(string(data))
+	//if err == nil {
+	//	offset = n
+	//}
+	//println(fmt.Sprintf("kafka offset begin: %d", offset))
+	//
+	//config := sarama.NewConfig()
+	//
+	//consumer, err := sarama.NewConsumer(addrs, config)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//r.conn = consumer
+	//
+	//pc, err := consumer.ConsumePartition(topic, int32(0), int64(offset))
+	//if err != nil {
+	//	panic(err)
+	//}
+	//r.partitionConsumer = pc
+
+	groupID := "group-1"
+	config := cluster.NewConfig()
+	config.Consumer.Return.Errors = false
+	config.Group.Return.Notifications = false
+	config.Consumer.Offsets.CommitInterval = 1 * time.Second
+	config.Consumer.Offsets.Initial = sarama.OffsetNewest
+
+	q.conn, err = cluster.NewConsumer(addrs, groupID, strings.Split(topic, ","), config)
 	if err != nil {
-		panic(err)
+		return
 	}
-	r.offsetFile = f
 
-	offset := 0
-	data, err := ioutil.ReadAll(f)
-	if err != nil {
-		panic(err)
-	}
-	n, err := strconv.Atoi(string(data))
-	if err == nil {
-		offset = n
-	}
-	println(fmt.Sprintf("kafka offset begin: %d", offset))
-
-	config := sarama.NewConfig()
-
-	consumer, err := sarama.NewConsumer(addrs, config)
-	if err != nil {
-		panic(err)
-	}
-	r.conn = consumer
-
-	pc, err := consumer.ConsumePartition(topic, int32(0), int64(offset))
-	if err != nil {
-		panic(err)
-	}
-	r.partitionConsumer = pc
-
-	return r
+	return
 }
 func (q *QueueConsumerKafka) Get() (log *Log, err error) {
+	//select {
+	//case msg, ok := <-q.partitionConsumer.Messages():
+	//	if ok {
+	//		log = &Log{}
+	//		err = json.Unmarshal(msg.Value, log)
+	//		if err != nil {
+	//			return
+	//		}
+	//
+	//		_, _ = q.offsetFile.Seek(0, 0)
+	//		_, _ = q.offsetFile.WriteString(strconv.FormatInt(msg.Offset+1, 10))
+	//	}
+	//case <-q.signals:
+	//	break
+	//}
 	select {
-	case msg, ok := <-q.partitionConsumer.Messages():
+	case msg, ok := <-q.conn.Messages():
 		if ok {
 			log = &Log{}
 			err = json.Unmarshal(msg.Value, log)
 			if err != nil {
 				return
 			}
-
-			_, _ = q.offsetFile.Seek(0, 0)
-			_, _ = q.offsetFile.WriteString(strconv.FormatInt(msg.Offset+1, 10))
+			q.conn.MarkOffset(msg, "")
 		}
 	case <-q.signals:
 		break
