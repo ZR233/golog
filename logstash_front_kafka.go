a14d5a0d8a12c07fadec500c9fc03d82b2c6feca
zrufo <zrufo747@outlook.com>
2019-11-12 17:46:59 +0800

    改进kafka存储

--- logstash_front_kafka.go
+++ logstash_front_kafka.go
@@ -57,7 +57,7 @@ func NewLogstashFrontKafka(config *LogstashFrontKafkaConfig) *LogstashFrontKafka
 	// 等待服务器所有副本都保存成功后的响应
 	saramaConfig.Producer.RequiredAcks = sarama.WaitForAll
 	// 随机的分区类型：返回一个分区器，该分区器每次选择一个随机分区
-	saramaConfig.Producer.Partitioner = sarama.NewManualPartitioner
+	saramaConfig.Producer.Partitioner = sarama.NewRandomPartitioner
 	// 是否等待成功和失败后的响应
 	saramaConfig.Producer.Return.Successes = true
 	producer, err := sarama.NewSyncProducer(config.KafkaAddrs, saramaConfig)
