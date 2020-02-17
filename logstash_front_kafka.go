dc3c2ef2177e15de9407ec57c53d1f35407aab56
zrufo <zrufo747@outlook.com>
2020-02-17 12:10:18 +0800

    改进kafka连接

--- logstash_front_kafka.go
+++ logstash_front_kafka.go
@@ -133,10 +133,10 @@ func (l *LogstashFrontKafka) restartKafka() (err error) {
 	}
 
 	saramaConfig := sarama.NewConfig()
-	saramaConfig.Net.DialTimeout = time.Second * 1
-	saramaConfig.Net.ReadTimeout = time.Second * 1
-	saramaConfig.Net.WriteTimeout = time.Second * 1
-	saramaConfig.Producer.Timeout = time.Second * 3
+	saramaConfig.Net.DialTimeout = time.Second * 5
+	saramaConfig.Net.ReadTimeout = time.Second * 5
+	saramaConfig.Net.WriteTimeout = time.Second * 5
+	saramaConfig.Producer.Timeout = time.Second * 5
 	// 等待服务器所有副本都保存成功后的响应
 	saramaConfig.Producer.RequiredAcks = sarama.WaitForAll
 	// 随机的分区类型：返回一个分区器，该分区器每次选择一个随机分区
